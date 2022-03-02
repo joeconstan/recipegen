ad15ac11ddbd73153465add5763c2ffbccb64ce5
Joe Constantine <jconstan@bu.edu>
2022-03-02 10:22:22 -0800

    v23 p2 - switched from modal to singlepage, started adding 'w/' pages

---
+++ src/app/w/eggs/eggs.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EggsComponent } from './eggs.component';
+
+describe('EggsComponent', () => {
+  let component: EggsComponent;
+  let fixture: ComponentFixture<EggsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EggsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EggsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
